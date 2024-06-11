e9352dd261d19a080896fb37a8729ac3d34b0190
narciso castro <narcisocastro0802@gmail.com>
2024-06-11 15:38:00 -0700

    Completed For Loop exercise

---
+++ 15Loops.cs
@@ -0,0 +1,75 @@
+for(int i = 0; i++)
+{
+Consoole.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++ )
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"$"{i});
+
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for(int i = 0; i++)
+{
+    Console.WriteLine($"{i}");
+} 
